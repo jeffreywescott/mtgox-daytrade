9eae0ae60f733909a7b917779657be00495caa2f
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-05-07 12:52:21 -0700

    ticker in place

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 var mtgox = require('node-mtgox-client-streaming');
 
-var API_KEY = '0ce0c4a6-fc1d-4cce-b912-9890e1bdbc4a';
-var API_SECRET = 'jV0xU+X3duqasJW/8FdXbrEg8BizdPwKIAFjk1678KMAiSaTcX08cV2jXWq9CunY7PpKBBY5NuSbrFLWK7Q97A==';
+if (!process.env.MTGOX_API_KEY || !process.env.MTGOX_API_SECRET) {
+  throw new Error("You must set your MTGOX_API_KEY and MTGOX_API_SECRET environment variables.");
+}
 
-var mtGox = new mtgox.MtGox(API_KEY, API_SECRET);
+
+var mtGox = new mtgox.MtGox(process.env.MTGOX_API_KEY, process.env.MTGOX_API_SECRET);
 mtGox.unsubscribe('trade');
 mtGox.unsubscribe('depth');
 //mtGox.unsubscribe('ticker');
 
-mtGox.onMessage(function(data) {
-  console.log(data);
+mtGox.authEmit({
+  call: 'private/info'
 });
 
-if (API_KEY && API_SECRET) {
-  mtGox.authEmit({
-    call: 'private/info'
-  });
-}
+
+var MtGoxStreamHandler = {
+  lastTickerValue: null,
+
+  private: function(data) {
+    switch(data.private) {
+      case 'ticker':
+        this.ticker(data);
+        break;
+      default:
+        console.log(data.op+"/"+data.private+" (ignored)");
+        break;
+    }
+  },
+
+  result: function(data) {
+      console.dir(data);
+  },
+
+  ticker: function(data) {
+    var ltv = parseFloat(data.ticker.last.value);
+    if (this.lastTickerValue != ltv) {
+      this.lastTickerValue = ltv;
+      console.log("ticker: 1BTC = USD $" + this.lastTickerValue);
+    }
+  },
+
+  default: function(data) {
+    if (data.op != 'subscribe' && data.op != 'unsubscribe') {
+      console.log(data.op + " (ignored)");
+    }
+  }
+
+};
+
+var lastTickerValue = null;
+
+mtGox.onMessage(function(data) {
+  if (data.op in MtGoxStreamHandler) {
+    MtGoxStreamHandler[data.op](data);
+  } else {
+    MtGoxStreamHandler.default(data);
+  }
+});
