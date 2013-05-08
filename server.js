a8d7b0e3bd71b8a88b688fabf2152dd39702c7d9
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-05-07 19:20:40 -0700

    store ticks in redis

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var mtgox = require('node-mtgox-client-streaming');
+var mtgox = require('node-mtgox-client-streaming'),
+    http  = require('http'),
+    redis = require('redis');
+
 
 if (!process.env.MTGOX_API_KEY || !process.env.MTGOX_API_SECRET) {
   throw new Error("You must set your MTGOX_API_KEY and MTGOX_API_SECRET environment variables.");
 }
 
-
 var mtGox = new mtgox.MtGox(process.env.MTGOX_API_KEY, process.env.MTGOX_API_SECRET);
-mtGox.unsubscribe('trade');
 mtGox.unsubscribe('depth');
+mtGox.unsubscribe('trade');
 //mtGox.unsubscribe('ticker');
 
 mtGox.authEmit({
   call: 'private/info'
+});
+
+
+var redisClient = redis.createClient(6379, 'nodejitsudb2457935228.redis.irstack.com');
+redisClient.auth('nodejitsudb2457935228.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
+ if (err) { throw err; }
 });
 
 
@@ -24,7 +32,7 @@ var MtGoxStreamHandler = {
         this.ticker(data);
         break;
       default:
-        if (data.private != 'depth') {
+        if (data.private != 'depth' && data.private != 'trade') {
           console.log(data.op+"/"+data.private+" (ignored)");
         }
         break;
@@ -39,6 +47,9 @@ var MtGoxStreamHandler = {
     var ltv = parseFloat(data.ticker.last.value);
     if (this.lastTickerValue != ltv) {
       this.lastTickerValue = ltv;
+      var dp = { t: (new Date()).getTime(), v: ltv };
+      redisClient.lpush("mtgox:ticks", JSON.stringify(dp));
+      redisClient.ltrim("mtgox:ticks", 0, 32767);
       console.log("ticker: 1BTC = USD $" + this.lastTickerValue);
     }
   },
@@ -60,3 +71,20 @@ mtGox.onMessage(function(data) {
     MtGoxStreamHandler.default(data);
   }
 });
+
+
+
+// creates a new httpServer instance
+http.createServer(function (req, res) {
+  // this is the callback, or request handler for the httpServer
+
+  // respond to the browser, write some headers so the 
+  // browser knows what type of content we are sending
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // write some content to the browser that your user will see
+  res.write('<h1>hello, i know nodejitsu.</h1>');
+
+  // close the response
+  res.end();
+}).listen(8080);
