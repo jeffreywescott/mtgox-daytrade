d788c27f27d31bcf412ddb412e604766e0146606
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-05-08 11:45:39 -0700

    better formatting

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-var mtgox = require('node-mtgox-client-streaming'),
-    http  = require('http'),
-    redis = require('redis');
+var app    = require('express')(),
+    server = require('http').createServer(app),
+    io     = require('socket.io').listen(server),
+    mtgox  = require('node-mtgox-client-streaming'),
+    redis  = require('redis');
 
 
 if (!process.env.MTGOX_API_KEY || !process.env.MTGOX_API_SECRET) {
   throw new Error("You must set your MTGOX_API_KEY and MTGOX_API_SECRET environment variables.");
 }
 
+// creates a new httpServer instance
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+});
+
+
 var mtGox = new mtgox.MtGox(process.env.MTGOX_API_KEY, process.env.MTGOX_API_SECRET);
 mtGox.unsubscribe('depth');
 mtGox.unsubscribe('trade');
 //mtGox.unsubscribe('ticker');
+// mtGox.authEmit({
+//   call: 'private/info'
+// });
 
-mtGox.authEmit({
-  call: 'private/info'
-});
-
-
-var redisClient = redis.createClient(6379, 'nodejitsudb2457935228.redis.irstack.com');
-redisClient.auth('nodejitsudb2457935228.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
- if (err) { throw err; }
-});
+var redisClient;
+var saveTick = function(dp) {
+  // only save things if we're in production
+  if (process.env.NODE_ENV === 'production') {
+    if (!redisClient) {
+      redisClient = redis.createClient(6379, 'nodejitsudb2457935228.redis.irstack.com');
+      redisClient.auth('nodejitsudb2457935228.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
+       if (err) { throw err; }
+      });
+    }
+    redisClient.lpush("mtgox:ticks", JSON.stringify(dp));
+    redisClient.ltrim("mtgox:ticks", 0, 32767);
+  }
+}
 
 
 var MtGoxStreamHandler = {
-  lastTickerValue: null,
-
   private: function(data) {
     switch(data.private) {
       case 'ticker':
@@ -45,13 +68,10 @@ var MtGoxStreamHandler = {
 
   ticker: function(data) {
     var ltv = parseFloat(data.ticker.last.value);
-    if (this.lastTickerValue != ltv) {
-      this.lastTickerValue = ltv;
-      var dp = { t: (new Date()).getTime(), v: ltv };
-      redisClient.lpush("mtgox:ticks", JSON.stringify(dp));
-      redisClient.ltrim("mtgox:ticks", 0, 32767);
-      console.log("ticker: 1BTC = USD $" + this.lastTickerValue);
-    }
+    var dp = { t: (new Date()).getTime(), v: ltv };
+    io.sockets.emit('tick', dp);
+    saveTick(dp);
+    console.log("ticker: 1BTC = USD $" + dp.v);
   },
 
   default: function(data) {
@@ -62,8 +82,6 @@ var MtGoxStreamHandler = {
 
 };
 
-var lastTickerValue = null;
-
 mtGox.onMessage(function(data) {
   if (data.op in MtGoxStreamHandler) {
     MtGoxStreamHandler[data.op](data);
@@ -71,20 +89,3 @@ mtGox.onMessage(function(data) {
     MtGoxStreamHandler.default(data);
   }
 });
-
-
-
-// creates a new httpServer instance
-http.createServer(function (req, res) {
-  // this is the callback, or request handler for the httpServer
-
-  // respond to the browser, write some headers so the 
-  // browser knows what type of content we are sending
-  res.writeHead(200, {'Content-Type': 'text/html'});
-
-  // write some content to the browser that your user will see
-  res.write('<h1>hello, i know nodejitsu.</h1>');
-
-  // close the response
-  res.end();
-}).listen(8080);
